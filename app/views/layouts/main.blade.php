b142eb3fce98091236fe3043745e9ab38dca35e3
Sheryar Shirazi <sheryarshirazi@gmail.com>
2015-09-28 19:39:49 +0500

    change link tag back to normal html

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -158,16 +158,17 @@
         </div><!-- end wrapper -->
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write("{{ HTML::script('js/vendor/jquery-1.9.1.min.js') }}")</script>
-        {{ HTML::script('js/plugins.js') }}
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script>//window.jQuery || document.write("{{ HTML::script('js/vendor/jquery-1.9.1.min.js') }}")</script>
+       {{ HTML::script('js/plugins.js') }}
         {{ HTML::script('js/main.js') }}
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            /*var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
+            s.parentNode.insertBefore(g,s)}(document,'script'));*/
         </script>
     </body>
 </html>
